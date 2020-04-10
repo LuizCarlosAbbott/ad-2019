2e0bd5a209e4636e7a688232ac181be01f474e94
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-04-09 21:32:23 -0300

    Update implementado

--- frontend/src/components/Persons/Persons.js
+++ frontend/src/components/Persons/Persons.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaTrash, FaPencilAlt } from "react-icons/fa";
 import { useMutation } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
@@ -13,33 +13,82 @@ const DELETE_PERSON = gql`
   }
 `;
 
+const UPDATE_PERSON = gql`
+  mutation updatePerson($id: String!, $input: PersonInput!) {
+    updatePerson(id: $id, input: $input) {
+      id
+      name
+      email
+    }
+  }
+`;
+
 function Persons(props) {
   const [deletePerson] = useMutation(DELETE_PERSON);
+  const [updatePerson] = useMutation(UPDATE_PERSON);
+  const [rIndex, setIndex] = useState(-1);
+  const [nameChange, setName] = useState("");
+  const [emailChange, setEmail] = useState("");
 
-  return props.data.persons.map(({ id, name, email }, index) => (
-    <tr key={id} id={id}>
-      <td>{id}</td>
-      <td>{name}</td>
-      <td>{email}</td>
-      <td>
-        <button
-          className="btn btn-warning" /* onClick={() => this.load("user")} */
-        >
-          <FaPencilAlt />
-        </button>
-        <button
-          className="btn btn-danger ml-2"
-          onClick={() => {
-            deletePerson({ variables: { id } });
-            const row = document.getElementById(id);
-            row.parentNode.removeChild(row);
-          }}
-        >
-          <FaTrash />
-        </button>
-      </td>
-    </tr>
-  ));
+  return props.data.persons.map(({ id, name, email }, index) => {
+    return (
+      <tr key={id} id={id}>
+        <td>{id}</td>
+        <td>
+          {rIndex !== index ? (
+            name
+          ) : (
+            <input
+              value={nameChange}
+              onChange={(e) => setName(e.target.value)}
+            />
+          )}
+        </td>
+        <td>
+          {rIndex !== index ? (
+            email
+          ) : (
+            <input
+              value={emailChange}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          )}
+        </td>
+        <td>
+          <button
+            className="btn btn-warning"
+            onClick={() => {
+              if (rIndex === index) {
+                updatePerson({
+                  variables: {
+                    id,
+                    input: { name: nameChange, email: emailChange },
+                  },
+                });
+                setIndex(-1);
+              } else {
+                setIndex(index);
+                setName(name);
+                setEmail(email);
+              }
+            }}
+          >
+            <FaPencilAlt />
+          </button>
+          <button
+            className="btn btn-danger ml-2"
+            onClick={() => {
+              deletePerson({ variables: { id } });
+              const row = document.getElementById(id);
+              row.parentNode.removeChild(row);
+            }}
+          >
+            <FaTrash />
+          </button>
+        </td>
+      </tr>
+    );
+  });
 }
 
 export default Persons;
