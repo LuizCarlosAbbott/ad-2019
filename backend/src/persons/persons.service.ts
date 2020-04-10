29f0641487e7057fd633ff76684cae7bfed4aacd
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-04-10 14:58:41 -0300

    Algoritimo amigo secreto

--- backend/src/persons/persons.service.ts
+++ backend/src/persons/persons.service.ts
@@ -36,8 +36,43 @@ export class PersonsService {
 
   async sortAndSend(): Promise<Person[]> {
     const persons = await this.personModel.find().exec();
-    console.log(persons);
+    const friends = Array(persons.length);
 
-    return persons;
+    for (let i = 0; i < persons.length; i++) {
+      const excluidos = [];
+      const auxPersons = [...persons];
+      if (i === 0) {
+        excluidos.push(persons[i]); // [Luiz]
+        auxPersons.slice(i, 1); // [Joel, Maria]
+
+        let valor = Math.floor(Math.random() * auxPersons.length);
+        friends[i] = auxPersons[valor]; // [ Jeol || Maria, empty, empty]
+      } else {
+        excluidos.push(persons[i]);
+        if (friends.includes(persons[i])) {
+          let indexF;
+          indexF = friends.indexOf(persons[i]);
+          excluidos.push(persons[indexF]);
+        }
+        excluidos.map(e => {
+          let indexEx = auxPersons.indexOf(e);
+          auxPersons.splice(indexEx, 1);
+        });
+        // console.log(auxPersons);
+
+        let valor = Math.floor(Math.random() * auxPersons.length);
+        friends[i] = auxPersons[valor];
+      }
+    }
+    console.log(persons, friends);
+    await persons.map(async ({ _id, name, email }, index) => {
+      await this.personModel.findByIdAndUpdate(
+        _id,
+        { name: name, email: email, friend: friends[index].name },
+        { new: true },
+      );
+    });
+
+    return await this.personModel.find().exec();
   }
 }
